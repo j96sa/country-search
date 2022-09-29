91e45db766f24a6f7caf1493042c173b899495ba
Josue Paulo Antuan <josue96sa@gmail.com>
2022-09-29 13:07:50 +0200

    feat(loading): error result **OK

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useEffect } from 'react';
 import loader_svg from "../assets/tail-spin.svg";
+import error_svg from "../assets/error_log.png";
 
 export default function Loading() {
   const [error, setError] = useState(false);
@@ -15,7 +16,8 @@ export default function Loading() {
   return (
     <>
       <div className={error ?"error" :"loading"}>        
-        <img src={error ?"https://www.freeiconspng.com/thumbs/error/red-circular-image-error-0.png" :loader_svg} alt="loader" />
+        <img src={error ?error_svg :loader_svg} alt="loader" />
+        {error && <h2>Sorry, we have not had any response from our server!</h2>}
       </div>
     </>
   )
